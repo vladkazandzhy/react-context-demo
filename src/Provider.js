e865a1bc4d7ff1a40a372280dbd70b51337a67ce
Vladyslav Victorovich Kazandzhi <kazandzhy@churchofjesuschrist.org>
2020-02-24 11:24:56 -0700

    added code

---
+++ src/Provider.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Char from './hero';
+
+const DEFAULT_STATE = {
+  allChar: Char,
+  searchTerm: '',
+};
+
+export const ThemeContext = React.createContext(DEFAULT_STATE);
+
+export default class Provider extends React.Component {
+  state = DEFAULT_STATE;
+
+  searchTermChanged = searchTerm => {
+    this.setState({searchTerm});
+  };
+
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{
+          ...this.state,
+          searchTermChanged: this.searchTermChanged,
+        }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Provider from './Provider';
+import Consumer from './Consumer';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+<Provider>
+  <Consumer>
+    <App />
+  </Consumer>
+</Provider>, 
+document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
